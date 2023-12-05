833bf46bcc4474facfca2d579f34e99fc9176525
wishon1 <wisdomhonest1@gmail.com>
2023-12-05 16:07:38 -0500

    add task 04

--- models/base_model.py
+++ models/base_model.py
@@ -1,11 +1,12 @@
-#!/usr/bin/python3
+#/usr/bin/python3
 """ module of the base class of Abhnb """
 import uuid
 from datetime import datetime
 
 class BaseModel():
+    
     """ the base class of Abhnb """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ 
         generate an id for each object and also 
         generate the time which each item will be created at 
@@ -13,11 +14,28 @@ class BaseModel():
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        
+        # check if kwargs is not emty
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, value)
+            
+            # convert the self.created_at and update_at to datetime object
+            if 'created_at' in kwargs:
+                self.created_at = datetime.strptime(kwargs['created_at'], 
+                                  '%Y-%m-%dT%H:%M:%S.%f')
+            if 'update_at' in kwargs:
+                self.update_at = datetime.strptime(kwargs['update_at'], 
+                                 '%Y-%m-%dT%H:%M:%S.%f')
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """ return the object in a specified format """
-        return "[{}] ({}) {}".format(
-                __class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """ 
