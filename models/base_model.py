6922a76f28a8b9f2cd0c90f4a948323823ea43ba
Ahmed <ahmedelsharkwiti@gmail.com>
2023-12-05 17:55:12 +0200

    MM

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,6 @@
 """ module of the base class of Abhnb """
 import uuid
 from datetime import datetime
-import datetime
 
 class BaseModel():
     """ the base class of Abhnb """
@@ -11,12 +10,12 @@ class BaseModel():
         generate an id for each object and also 
         generate the time which each item will be created at 
         """
-        self.id = uuid.uuid4()
-        self.created_at = datetime.datetime.now()
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """ return the object in a specified format """
-        self.created_at = repr(self.created_at)
         return "[{}] ({}) {}".format(__class__.__name__, self.id, self.__dict__)
 
     def save(self):
@@ -24,16 +23,15 @@ class BaseModel():
         updates the public instance attribute 
         updated_at with the current datetime
         """
-        self.updated_at = datetime.datetime.now()
-        self.updated_at = repr(self.updated_at)
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """ 
         returns a dictionary containing all keys/values of 
         __dict__ of the instance
         """
-        self.created_at = datetime.strptime(self.created_at, "%Y-%m-%dT%H:%M:%S.%f")
-        self.updated_at = datetime.strptime(self.created_at, "%Y-%m-%dT%H:%M:%S.%f")
+        self.created_at = self.created_at.isoformat()
+        self.updated_at = self.updated_at.isoformat()
         dict = self.__dict__
         dict["__class__"] = __class__.__name__
         return dict
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python3
 """ test case for the base model class """
+import io
+import sys
 import unittest
-
+from models.base_model import BaseModel
 class Test_Base_model_class(unittest.TestCase):
     """ Test Base model class """
-    my_model = BaseModel()
-    self.assertEqual(my_model.id, uuid.uuid4())
-    self.assertEqual(my_model.created_at, 
+    def test_str(self):
+        capturedOutput = io.StringIO()
+        sys.stdout = capturedOutput
+
+        my_model = BaseModel()
+        print(my_model)
+
+        my_model.save()
+        print(my_model)
+
+        #print( my_model.to_dict())
+
+        sys.stdout = sys.__stdout__
+        self.assertEqual(capturedOutput.getvalue(
+            ), f"{my_model.__class__.__name__}, {my_model.id}, {my_model.__dict__}\n",
+               f"{my_model.__class__.__name__}, {my_model.id}, {my_model.__dict__}")
