83a303a98aad892c764d6a9e7a9f093bb0321837
Ahmed <ahmedelsharkwiti@gmail.com>
2023-12-09 16:38:47 +0200

    MM

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -23,19 +23,22 @@ class FileStorage():
     def new(self, obj):
         """ sets in __objects the obj with key <obj class name>.id """
         element = "{}.{}".format(obj.__class__.__name__, obj.id)
-        FileStorage.__objects.update({element:obj})
+        FileStorage.__objects.update({element: obj})
 
     def save(self):
-        """  
+        """
         serializes __objects to the JSON file (path: __file_path)
-        i created new_objects dictionary in different location becuase 
+        i created new_objects dictionary in different location becuase
         when we iterate over __objects dictionary we will an error which
-        is value has no to_dict method because that value will be dictionary no object
-        that thing will happen when we create an object for first time but it happen
+        is value has no to_dict method because that
+        value will be dictionary no object
+        that thing will happen when
+        we create an object for first time but it happen
         when we create an object for the second time
         """
         new_objects = FileStorage.__objects.copy()
-        for key, value in  FileStorage.__objects.items():
+
+        for key, value in FileStorage.__objects.items():
             new_objects[key] = value.to_dict()
         with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
             json.dump(new_objects, f)
@@ -43,7 +46,7 @@ class FileStorage():
     def reload(self):
         """Deserialize the JSON file"""
         """ deserializes the JSON file to __objects """
-        if os.path.isfile(FileStorage.__file_path) == False:
+        if os.path.isfile(FileStorage.__file_path) is False:
             return
         else:
             with open(FileStorage.__file_path, "r", encoding="utf-8") as file:
